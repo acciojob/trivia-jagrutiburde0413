4f04c89037bc0f8784fe307ba9f5f864e4ed2168
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-02-09 00:11:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
 //your code here
-div{
-	
-}
-#capital{
-	max-width: 600px;
-    margin : 10px;
-   padding:15px;
-   border-left: 8px solid green;
-   background-color: #f8fffe;
-   font-weight: bold;
-}
+
